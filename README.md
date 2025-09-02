1eba4684571cad4ca8e85bc042d5529c7b514ed0
Omar <38073753+lordsayur@users.noreply.github.com>
2025-09-02 23:16:45 +0000

    implement text conversion

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # wsb-prayer-importer
+
+Convert data copied from image taken from Wakstu Sembahyang Brunei Darussalam
+whatsapp channel to formatted prayer data supported by WSB
+
+## How to
+
+1. Copy text from image (using OCR)
+1. Paste text to [prayer-data/prayer_times.txt](./prayer-data//prayer_times.txt)
+1. Run on terminal `deno task start` to start conversion
+   - you will be prompted to specify some details (hijri and masihi date)
+1. Once done, result could be found on
+   [prayer-data/prayer-formatted.txt](./prayer-data//prayer-formatted.txt)
+   - Please confirm hijri year if there is new year. You will need to manually
+     update the hijri year
+1. Copy the text and paste to [WSB](wsb.netlify.app/admin)
--- main.ts
+++ main.ts
@@ -1,1 +1,51 @@
-console.log('hello')
+const masihiMonth = prompt("Specify masihi month as number") || 9;
+const masihiYear = prompt("Specify masihi year as number") || 2025;
+const hijriMonth1 = prompt("Specify first hijri month as text") || "Rabiulawal";
+const hijriMonth2 = prompt("Specify second hijri month as text") ||
+  "Rabiulakhir";
+const hijriYear = prompt("Specify hijri year as number") || "1447";
+
+const content = await Deno.readTextFile("./prayer-data/prayer_times.txt");
+// console.log(content);
+const lines = content.split("\n");
+
+const days = ["Selasa", "Rabu", "Khamis", "Jumaat", "Sabtu", "Ahad", "Isnin"];
+
+const linesByDay: string[][] = [];
+let day: string[] = [];
+
+lines.forEach((line, index) => {
+  if (days.includes(line)) {
+    if (day.length) linesByDay.push(day);
+    day = [];
+    day.push(line);
+  } else {
+    day.push(line);
+  }
+
+  if (index === lines.length - 1) {
+    linesByDay.push(day);
+  }
+});
+
+let previousHijriDay = "";
+let currentHijriMonth = hijriMonth1;
+const text = linesByDay
+  .map((d) => {
+    if (previousHijriDay !== "" && +previousHijriDay > +d[2]) {
+      currentHijriMonth = hijriMonth2;
+    }
+
+    previousHijriDay = d[2];
+
+    return `${d[1]}/${masihiMonth}/${masihiYear}\t${
+      d[2]
+    } ${currentHijriMonth} ${hijriYear}\t${d[3]}\t${d[4]}\t${d[5]}\t${d[6]}\t${
+      d[7]
+    }\t${d[8]}\t${d[9]}\t${d[10]}`;
+  })
+  .join("\n");
+
+await Deno.writeTextFile("./prayer-data/prayer-formatted.txt", text);
+
+console.log("File saved!");
